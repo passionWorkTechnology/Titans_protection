77f476257bfe5952b03629f5b5712a5f7d8c4435
passionWorkTechnology <floriandeveloppement22@gmail.com>
2025-09-03 10:59:32 +0200

    add guinicorn at requirements.txt

--- titans/settings.py
+++ titans/settings.py
@@ -62,20 +62,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'titans.wsgi.application'
 
 # -------------------
-# DATABASE
-# -------------------
-DATABASE_URL = os.environ.get("DATABASE_URL")
-if DATABASE_URL:
-    DATABASES = {
-        "default": dj_database_url.parse(DATABASE_URL, conn_max_age=600, ssl_require=True)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+}
 
 # -------------------
 # PASSWORD VALIDATION
